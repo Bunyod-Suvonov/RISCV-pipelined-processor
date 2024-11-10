c088331d6b19dbf9984cb8591197e1886b6fee1e
Bunyod-Suvonov <b.suvonov2022@gmail.com>
2024-11-10 18:04:17 +0800

    push files

---
+++ pipelined_processor/pipelined_processor.srcs/sources_1/new/Adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/10/2024 01:33:36 PM
+// Design Name: 
+// Module Name: Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Adder(
+
+    );
+endmodule
